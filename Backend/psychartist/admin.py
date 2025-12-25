d29a6254146f0e6b64709bf32da7b018da5f56ba
MadhanKumar-py <madhankumar10523@gmail.com>
2025-12-26 02:09:11 +0530

    Test: Enable both Psychartist admin models

--- Backend/psychartist/admin.py
+++ Backend/psychartist/admin.py
@@ -3,3 +3,4 @@ from .models import PsychartistApplication, Psychartist
 
 # Enable one by one to identify 500 error source
 admin.site.register(PsychartistApplication)
+admin.site.register(Psychartist)
